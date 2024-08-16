1651457e01c9fb540787e97c907d5a14dfc29796
AKira0169 <odayterk75@gmail.com>
2024-08-16 22:15:11 +0300

    fixing

--- client/src/components/Api/Endpointes.tsx
+++ client/src/components/Api/Endpointes.tsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:3000/api/v1";
+export const BASE_URL = "http://localhost:3000/api/v1/";
 // http://127.0.0.1:3000/api/v1/users/signUp
 export const USERS = "users";
 export const SIGN_UP = "signUp";
