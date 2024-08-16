d9dc7c612695564a5aa65b5f48062793168c1e7c
Ahmed-Elshhat <a7medelshhat@gmail.com>
2024-08-16 21:15:23 +0300

    rahama3

--- client/src/components/Api/Endpointes.tsx
+++ client/src/components/Api/Endpointes.tsx
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "http://localhost:3000/api/v1/";
+export const USERS = "users";
+export const SIGN_UP = "signUp";
+export const LOGIN = "logIn";
