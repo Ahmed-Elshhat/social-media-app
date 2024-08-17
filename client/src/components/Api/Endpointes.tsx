ad9f60141ca7743ca3a33c52a39fced43d9ae7ce
Ahmed-Elshhat <a7medelshhat@gmail.com>
2024-08-17 16:15:19 +0300

    Edit on login page style and logic

--- client/src/components/Api/Endpointes.tsx
+++ client/src/components/Api/Endpointes.tsx
@@ -1,5 +1,5 @@
+// https://www.youtube.com/watch?v=98On6EV4Th0
 export const BASE_URL = "http://127.0.0.1:3000/api/v1";
-// http://127.0.0.1:3000/api/v1/users/signUp
 export const USERS = "users";
 export const SIGN_UP = "signUp";
 export const LOGIN = "logIn";
